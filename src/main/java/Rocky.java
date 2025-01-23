dc870298ffd1dc34dc25a61a9dd334b774c2269d
UTMC_PC24 <UTMC_PC24@UTowns-iMac-7.local>
2025-01-23 13:57:14 +0800

    Updated Rocky.java with new content

--- src/main/java/Rocky.java
+++ src/main/java/Rocky.java
@@ -1,10 +1,36 @@
-public class Eridian {
+class Color {
+    public static final String none = "\033[m";
+
+    public static final String green = "\033[32m";
+    public static final String purple = "\033[35m";
+}
+
+public class Rocky {
+    private static final String logo = "██████╗  ██████╗  ██████╗██╗  ██╗██╗   ██╗\n"
+                                     + "██╔══██╗██╔═══██╗██╔════╝██║ ██╔╝╚██╗ ██╔╝\n"
+                                     + "██████╔╝██║   ██║██║     █████╔╝  ╚████╔╝\n"
+                                     + "██╔══██╗██║   ██║██║     ██╔═██╗   ╚██╔╝\n"
+                                     + "██║  ██║╚██████╔╝╚██████╗██║  ██╗   ██║\n"
+                                     + "╚═╝  ╚═╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝   ╚═╝\n";
+
+    private static final String introduction = "Hello, I'm Rocky\n" +
+                                                "What can I do for you?";
+
+    private static void say(String message) {
+        System.out.println(Color.green);
+
+        System.out.println("<<<<<<<");
+        System.out.println(message);
+        System.out.println(">>>>>>>");
+
+        System.out.println(Color.none);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        say(introduction);
+
+        say("Bye. Hope to see you again soon!");
+        System.exit(0);
     }
 }
